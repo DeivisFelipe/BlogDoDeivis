0a50ca1937ec87f43139a728adfb069ed276b889
DeivisFelipe <guerreirodosul01@gmail.com>
2025-08-11 14:05:32 -0300

    fix: corrigindo uns erros

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-
 require_once __DIR__ . '/vendor/autoload.php';
 
-use Config\Database;
+use App\Config\Database;
 
 $db = new Database();
 $connection = $db->getConnection();
-echo "Database connection established successfully.";
+
+
